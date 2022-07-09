530b94a1c1d2e1e9207849e6a7c1fa2be80db7c3
danilsonplacid <denilsonplcid@gmail.com>
2022-07-09 12:12:12 +0600

    folder changens

---
+++ src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header>
+        <div class="container react" id="header">
+          <div>
+            <img class="logo" src="images/logo.svg" alt="Logo" />
+          </div>
+          <div class="textRight">
+            <img src="images/react.svg" alt="React" class="reactLogo" /><strong
+              >React</strong
+            >
+          </div>
+        </div>
+      </header>
+  )
+}
+
+export default header
